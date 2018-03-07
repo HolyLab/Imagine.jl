8b25a9bb998e4086796e5fed351c51786a586ccd
Cody-G <codyjgreer@gmail.com>
2018-03-07 15:03:08 -0600

    comment out lines that require hardware so that package can load as a dependency

--- src/Imagine.jl
+++ src/Imagine.jl
@@ -4,18 +4,18 @@ module Imagine
 
 using ImagineInterface, Unitful
 import ImagineWorker
-using NIDAQ #just for the trigger pulse
-
-const NPERSISTENT_WORKERS = 4 #one each for AI, AO, DI, DO, one for PFI pulse
-DEFAULT_DEVICE = NIDAQ.devices()[1] * "/" #"Dev1/"
-FREE_WORKERS = Int[]
-USED_WORKERS = Int[]
-WORKERS = Int[]
-
-export run_imagine,
-        set_device
-
-include("manage_workers.jl")
-include("io.jl")
-include("run.jl")
+#using NIDAQ #just for the trigger pulse
+#
+#const NPERSISTENT_WORKERS = 4 #one each for AI, AO, DI, DO, one for PFI pulse
+#DEFAULT_DEVICE = NIDAQ.devices()[1] * "/" #"Dev1/"
+#FREE_WORKERS = Int[]
+#USED_WORKERS = Int[]
+#WORKERS = Int[]
+#
+#export run_imagine,
+#        set_device
+#
+#include("manage_workers.jl")
+#include("io.jl")
+#include("run.jl")
 end

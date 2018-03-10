0644fd725200c2ed294057a11eaffe2c7182bee4
Cody-G <codyjgreer@gmail.com>
2018-03-10 13:59:06 -0600

    Allow module to load without NIDAQ (it's not functional in that case)

--- src/Imagine.jl
+++ src/Imagine.jl
@@ -1,21 +1,26 @@
-__precompile__(true)
+__precompile__(false)
 
 module Imagine
 
 using ImagineInterface, Unitful
 import ImagineWorker
-using NIDAQ #just for the trigger pulse
 
-const NPERSISTENT_WORKERS = 4 #one each for AI, AO, DI, DO, one for PFI pulse
-DEFAULT_DEVICE = NIDAQ.devices()[1] * "/" #"Dev1/"
-FREE_WORKERS = Int[]
-USED_WORKERS = Int[]
-WORKERS = Int[]
+if is_windows() && isfile("C:\\Windows\\System32\\nicaiu.dll")
+    using NIDAQ #just for the trigger pulse
+    const NPERSISTENT_WORKERS = 4 #one each for AI, AO, DI, DO, one for PFI pulse
+    DEFAULT_DEVICE = NIDAQ.devices()[1] * "/" #"Dev1/"
+    FREE_WORKERS = Int[]
+    USED_WORKERS = Int[]
+    WORKERS = Int[]
 
-export run_imagine,
-        set_device
+    export run_imagine,
+            set_device
 
-include("manage_workers.jl")
-include("io.jl")
-include("run.jl")
+    include("manage_workers.jl")
+    include("io.jl")
+    include("run.jl")
+else
+    warn("DAQ unavailable. The Imagine package will not work")
+end
+
 end
